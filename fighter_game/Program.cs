0da7b75a3edbba7958430fdca2ea81eacff20e59
Frejbo <frej.hellsten@gmail.com>
2022-09-27 12:15:00 +0200

    Buggfixar och ökad hälsa

--- fighter_game/Program.cs
+++ fighter_game/Program.cs
@@ -1,10 +1,10 @@
-﻿int enemy_hp = 20;
-int my_hp = 20;
+﻿int enemy_hp = 30;
+int my_hp = 30;
 
 Random random = new Random();
 
 while (enemy_hp > 0 && my_hp > 0) {
-    Console.WriteLine($"Du har {my_hp} hp.");
+    Console.WriteLine($"\nDu har {my_hp} hp.");
     Console.WriteLine($"Din fiende har {enemy_hp} hp.");
     Console.ReadLine();
     
@@ -32,27 +32,32 @@ while (enemy_hp > 0 && my_hp > 0) {
         int axe_hurt_rate = random.Next(0, 5);
         if (axe_hurt_rate == 0) {
             Console.WriteLine("Du missade! Din fiende tog 0 i skada.");
-        } else if (axe_hurt_rate > 0 && !(enemy_hp >= 4)) {
-            Console.WriteLine("Du högg din fiende i armen och hen tappade 3 hp.");
-            enemy_hp -= 3;
         } else if (axe_hurt_rate == 4) {
             Console.WriteLine("Du drog ett skitbra drag och träffade din fiendes hals, hen tappade 5 hp!");
             enemy_hp -= 5;
         } else if (axe_hurt_rate == 5) {
             Console.WriteLine("Attans! Din fiende tog fram sin sköld och dämpade ditt slag. Varken du eller din fiende gjorde någon skada.");
             continue;
+        } else {
+            Console.WriteLine("Du högg din fiende i armen och hen tappade 3 hp.");
+            enemy_hp -= 3;
         }
     } else if (my_chosen_weapon == 3) {
-        Console.WriteLine("Vem fan fightas med en hammare? Din fiende blev förvirrad och tappade sitt vapen. Du drog ett hårt slag och din fiende tappade 2 hp.");
-        Console.WriteLine("Din fiende gjorde ingen skada på dig.");
-        enemy_hp -= 2;
-        continue;
+        int hammer_hurt_rate = random.Next(0, 1);
+        if (hammer_hurt_rate == 0) {
+            Console.WriteLine("Vem fan fightas med en hammare? Din fiende blev förvirrad och tappade sitt vapen. Du drog ett hårt slag och din fiende tappade 2 hp.");
+            Console.WriteLine("Din fiende gjorde ingen skada på dig.");
+            enemy_hp -= 2;
+            continue;
+        } else {
+            Console.WriteLine("Du slog din fiende i magen med hammaren men hen tappade endast 1 hp.");
+        }
     } else if (my_chosen_weapon == 4) {
         Console.WriteLine("Du tog fram din kniv och högg din fiende i benet. Din fiende tappade 3 hp.");
         enemy_hp -= 3;
     }
 
-    int my_pain = random.Next(0, 5);
+    int my_pain = random.Next(0, 3);
     if (my_pain == 0) {
         Console.WriteLine("Din fiende missade och högg i marken!");
         Console.WriteLine("Du tog ingen skada.");
@@ -67,8 +72,12 @@ while (enemy_hp > 0 && my_hp > 0) {
         Console.WriteLine("Du tappade 3 hp.");
     }
     my_hp -= my_pain;
+}
 
-    Console.WriteLine(enemy_hp);
+if (enemy_hp <= 0) {
+    Console.WriteLine("Du vann mot din fiende!");
+} else if (my_hp <= 0) {
+    Console.WriteLine("Du föll ihop på marken och förblödde plågsamt. Du förlorade.");
 }
 
 
